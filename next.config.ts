d1a790c1251d0f31aafb5f5643cb26c5c16597b7
Mohsanul786 <mdmohsan2407@gmail.com>
2025-09-25 17:38:55 +0530

    fix: image error in prod

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "randomuser.me",
-        pathname: "/**",
+        pathname: "/api/portraits/**",
       },
     ],
   },
